8cbb0cce4b8ae9e46666a854b890272b6a799fda
Minyasei <139749656+Minyasei@users.noreply.github.com>
2024-08-08 16:28:31 +0300

    polls url cleaned and os.BASE_DIR added still have choice_set problem

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,26 +1,25 @@
 from django.db.models import F
-
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
-
 from .models import Question, Choice
 
+def get_question_or_404(question_id):
+    return get_object_or_404(Question, pk=question_id)
 
-def index(request): # type: ignore
-    return HttpResponse("Hello, world. You're at the polls index.")
+def detail_or_results(request, question_id, template_name):
+    question = get_question_or_404(question_id)
+    return render(request, template_name, {"question": question})
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    return detail_or_results(request, question_id, "polls/detail.html")
 
 def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+    return detail_or_results(request, question_id, "polls/results.html")
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    question = get_question_or_404(question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
@@ -37,6 +36,7 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
 
+#display
 def index(request):
     latest_question_list = Question.objects.all()
     context = {'latest_question_list': latest_question_list}
